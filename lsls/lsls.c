35805158e3d0db133cf8ecca4a18de050df6853c
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-06-22 12:25:23 -0400

    Robert Hogan. Project completed.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h> // header file standard io
+#include <string.h> // header file header defines one variable type, one macro, and various functions for manipulating arrays of characters.
+#include <stdlib.h>// header file standard library
 #include <dirent.h>
+//The <dirent.h> header defines the following data type through typedef:
+//DIR
+//A type representing a directory stream.
+//It also defines the structure dirent which includes the following members:
 #include <sys/types.h>
 
 
@@ -10,25 +14,25 @@
  */
 int main(int argc, char **argv)
 {
-  char *name;
+  char *name; // pointer to name
   // Parse command line
   if(argc > 1)
   {
-    name = strdup(argv[1]);
+    name = strdup(argv[1]); //The strdup() function is used to duplicate a string //copying value argv[1] setting to name;
   } else 
   {
-    name = ".";
+    name = "."; // if there is no arg[v], the value is "."
   }
  
   // Open directory
-  DIR *dir;
-  struct dirent *sd;
-  dir = opendir(name);
+  DIR *dir; //The DIR data type represents a directory stream. *dir is a pointer to a directory stream.
+  struct dirent *sd; // sd is now set to a new type of data. created by a struct. 
+  dir = opendir(name); // location of the directory to open with the *dir pointer. open the directory with the name provided. this sets dir to a function that opens the passed in directory. 
 
-  if(dir == NULL)
+  if(dir == NULL) // if there is no directory...print the following. 
   {
-    printf("Unable to open the directory. Were sorry!");
-    exit(1);
+    printf("Unable to open the directory. Were sorry!"); // print this
+    exit(1); // exit. 
   }
 
   // Repeatly read and print entries
